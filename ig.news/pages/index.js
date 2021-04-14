01abf0afb53229d75cc60d21670f75b053f078e4
Diebraga <diebraga0055@gmail.com>
2021-04-14 23:06:55 +0100

    first commit

---
+++ ig.news/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>ig.news</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      First commit
+    </div>
+  )
+}
