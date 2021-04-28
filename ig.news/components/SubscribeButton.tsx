f32f5143050aa069620e7792af87ed619332aa5e
diego braga <diebraga0055@gmail.com>
2021-04-28 22:35:47 +0100

    stripe subscribe buttn

--- ig.news/components/SubscribeButton.tsx
+++ ig.news/components/SubscribeButton.tsx
@@ -1,8 +1,39 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../services/api'
+import { getStripe } from '../services/stripe-js'
 import styles from '../styles/subscribebutton.module.scss'
 
-export function SubscribeButton() {
+interface subscribeButtonProps {
+  priceId: string
+} 
+
+export function SubscribeButton({ priceId }: subscribeButtonProps) {
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripe()
+
+      await stripe.redirectToCheckout({ sessionId: sessionId })
+    } catch (error) {
+      alert(error.message)
+      
+    }
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}>
       Subscribe Now
     </button>
   )
