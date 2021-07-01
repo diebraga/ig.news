1075fe3f21cb36addd43eebd1d4e3f8e10f19d2f
diebraga <diebraga0055@gmail.com>
2021-07-01 11:31:26 +0100

    deploy test 3

--- services/prismic.ts
+++ services/prismic.ts
@@ -2,7 +2,7 @@ import Prismic from '@prismicio/client'
 
 export function getPrismicClient(req?: unknown) {
   const prismic = Prismic.client(
-    process.env.PRISIC_ENDPOINT,
+    'https://ignewsdev77.cdn.prismic.io/api/v2',
     {
       req,
       accessToken: process.env.PRISIC_ACCESS_TOKEN
